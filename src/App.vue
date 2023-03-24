2771518c27d4ce15cec177c17dbe72af2d841ff1
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2023-03-24 22:08:43 +0600

    update video iframe

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,15 @@
   <div class="mx-auto h-screen grid grid-cols-12 grid-rows-4">
     <div class="bg-red-900 row-span-6 col-span-9 ">01</div>
     <div class="bg-blue-900 row-span-1 col-span-3">
-      <iframe class="w-full h-full" src="https://player.twitch.tv/?channel=melharucos&parent=super-genie-c064d2.netlify.app"></iframe>
+      <iframe class="w-full h-full"
+              allowfullscreen="false"
+              autoplay="true"
+              muted="true"
+              src="https://player.twitch.tv/?channel=melharucos&parent=super-genie-c064d2.netlify.app"></iframe>
     </div>
     <div class="bg-green-900 row-span-3 col-span-3">
-      <iframe class="w-full h-full" src="https://www.twitch.tv/embed/melharucos/chat?parent=super-genie-c064d2.netlify.app"></iframe>
+      <iframe class="w-full h-full"
+              src="https://www.twitch.tv/embed/melharucos/chat?parent=super-genie-c064d2.netlify.app"></iframe>
     </div>
   </div>
 </template>
